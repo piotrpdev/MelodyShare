cac96ce94b0b9e43b0cb722dfa286fde60cbee03
piotrpdev <piotrpdev@gmail.com>
2024-01-17 15:59:22 +0000

    feat: button, different theme, custom strings

--- app/src/main/java/ie/setu/mobileassignment/MainActivity.kt
+++ app/src/main/java/ie/setu/mobileassignment/MainActivity.kt
@@ -2,11 +2,19 @@ package ie.setu.mobileassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     // This is a test comment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickMe(view: View) {
+        Toast.makeText(this,
+            R.string.message,
+            Toast.LENGTH_LONG).show()
     }
 }
