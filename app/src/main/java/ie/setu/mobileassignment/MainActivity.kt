63b7f2063cbacd57db2da703a182de6f3e4796af
piotrpdev <piotrpdev@gmail.com>
2024-01-24 15:55:11 +0000

    feat: logger and button press count

--- app/src/main/java/ie/setu/mobileassignment/MainActivity.kt
+++ app/src/main/java/ie/setu/mobileassignment/MainActivity.kt
@@ -1,20 +1,31 @@
 package ie.setu.mobileassignment
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import ie.setu.mobileassignment.utils.log
 
 class MainActivity : AppCompatActivity() {
+    var buttonPressedCount: Int = 0
+
     // This is a test comment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    @SuppressLint("SetTextI18n")
     fun clickMe(view: View) {
+        buttonPressedCount++
+
         Toast.makeText(this,
             R.string.message,
             Toast.LENGTH_LONG).show()
+
+        findViewById<TextView>(R.id.btnTextView).text = "Button pressed $buttonPressedCount time(s)"
+        log.info{"Button pressed $buttonPressedCount time(s)"}
     }
 }
---
+++ app/src/main/java/ie/setu/mobileassignment/utils/Logger.kt
@@ -0,0 +1,5 @@
+package ie.setu.mobileassignment.utils
+
+import mu.KotlinLogging
+
+val log = KotlinLogging.logger {}
