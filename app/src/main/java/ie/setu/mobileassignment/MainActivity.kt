72b19ae3f9fc1101a62e0278d2aedca527251861
piotrpdev <piotrpdev@gmail.com>
2024-01-31 20:16:40 +0000

    refactor: change theme button to a MaterialSwitch, switch logging to Timber, add Application()

--- app/src/main/java/ie/setu/mobileassignment/MainActivity.kt
+++ app/src/main/java/ie/setu/mobileassignment/MainActivity.kt
@@ -7,32 +7,37 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import com.github.ajalt.timberkt.Timber
 import com.github.ajalt.timberkt.i
-import timber.log.Timber.i
+import com.google.android.material.materialswitch.MaterialSwitch
 
 class MainActivity : AppCompatActivity() {
     private var buttonPressedCount: Int = 0
+
+    private fun updateSwitchBasedOnTheme() {
+        val themeSwitch = findViewById<MaterialSwitch>(R.id.themeSwitch)
+        themeSwitch.isSaveEnabled = false
+
+        when ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                i { "Dark mode detected, changing themeSwitch isChecked to true" }
+                themeSwitch.isChecked = true
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                i { "Light mode detected, changing themeSwitch isChecked to false" }
+                themeSwitch.isChecked = false
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
+        }
+    }
 
     // This is a test comment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Timber.plant(Timber.DebugTree())
-        i("MainActivity started..")
+        i { "MainActivity started." }
 
-        when ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK)) {
-            Configuration.UI_MODE_NIGHT_YES -> {
-                findViewById<TextView>(R.id.themeBtn).text = getString(R.string.lightModeBtnText)
-                i { "Dark mode detected, changing theme button text" }
-            }
-            Configuration.UI_MODE_NIGHT_NO -> {
-                findViewById<TextView>(R.id.themeBtn).text = getString(R.string.darkModeBtnText)
-                i { "Light mode detected, changing theme button text" }
-            }
-            Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
-        }
+        updateSwitchBasedOnTheme()
     }
 
     fun clickMe(view: View) {
@@ -45,11 +50,12 @@ class MainActivity : AppCompatActivity() {
         val message = getString(R.string.button_pressed_time_s, buttonPressedCount)
 
         findViewById<TextView>(R.id.btnTextView).text = message
-        i{message}
+        i { message }
     }
 
-    fun themeBtnClick(view: View) {
+    fun onThemeSwitchToggle(view: View) {
         var currentlyDark = false
+
         when ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK)) {
             Configuration.UI_MODE_NIGHT_YES -> {
                 currentlyDark = true
@@ -60,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
         }
 
-        i { "UI_MODE_NIGHT is set to $currentlyDark, attempting to invert..." }
+        i { "UI_MODE_NIGHT is currently set to $currentlyDark, attempting to invert..." }
         AppCompatDelegate.setDefaultNightMode(if (currentlyDark) AppCompatDelegate.MODE_NIGHT_NO else AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
