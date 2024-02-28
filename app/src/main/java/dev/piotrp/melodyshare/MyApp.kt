908a69ad132503de63598f941a3df14ee8d22ac5
piotrpdev <piotrpdev@gmail.com>
2024-02-28 20:01:33 +0000

    fix: missing import in MyApp

--- app/src/main/java/dev/piotrp/melodyshare/MyApp.kt
+++ app/src/main/java/dev/piotrp/melodyshare/MyApp.kt
@@ -3,6 +3,7 @@ package dev.piotrp.melodyshare
 import android.app.Application
 import com.github.ajalt.timberkt.Timber
 import dev.piotrp.melodyshare.models.PlacemarkModel
+import timber.log.Timber.i
 
 class MyApp  : Application() {
     val placemarks = ArrayList<PlacemarkModel>()
