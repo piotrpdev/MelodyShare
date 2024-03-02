2fef2f451ed72470b6f5cb7376df033a8c3e4583
piotrpdev <piotrpdev@gmail.com>
2024-03-02 20:14:53 +0000

    feat: make PlacemarkModel parcelable

--- app/src/main/java/dev/piotrp/melodyshare/models/PlacemarkModel.kt
+++ app/src/main/java/dev/piotrp/melodyshare/models/PlacemarkModel.kt
@@ -1,3 +1,7 @@
 package dev.piotrp.melodyshare.models
 
-data class PlacemarkModel(var title: String = "", var description: String = "")
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PlacemarkModel(var title: String = "", var description: String = "") : Parcelable
