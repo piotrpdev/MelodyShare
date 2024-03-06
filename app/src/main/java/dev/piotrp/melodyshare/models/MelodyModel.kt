2fdcd65abfdf5fb5b623b7b8ff08d65868d04ba7
piotrpdev <piotrpdev@gmail.com>
2024-03-06 23:16:58 +0000

    feat: add tempo property to Melody

--- app/src/main/java/dev/piotrp/melodyshare/models/MelodyModel.kt
+++ app/src/main/java/dev/piotrp/melodyshare/models/MelodyModel.kt
@@ -4,4 +4,4 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class MelodyModel(var id: Long = 0, var title: String = "", var description: String = "", var notes: ArrayList<MelodyNote> = ArrayList()) : Parcelable
+data class MelodyModel(var id: Long = 0, var title: String = "", var description: String = "", var tempo: Float = 120f, var notes: ArrayList<MelodyNote> = ArrayList()) : Parcelable
