47936986dba00de519cd236fa44c22a1713ae349
piotrpdev <piotrpdev@gmail.com>
2024-03-06 23:11:40 +0000

    refactor: remove channel property from MelodyNote since there will only ever be one instrument

--- app/src/main/java/dev/piotrp/melodyshare/models/MelodyNote.kt
+++ app/src/main/java/dev/piotrp/melodyshare/models/MelodyNote.kt
@@ -4,5 +4,5 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class MelodyNote(var channel: Int, var pitch: Int, var velocity: Int, var tick: Long, var duration: Long) :
+data class MelodyNote(var pitch: Int, var velocity: Int, var tick: Long, var duration: Long) :
     Parcelable
