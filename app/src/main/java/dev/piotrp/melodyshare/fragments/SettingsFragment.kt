ad42875416612a56fc2b8f53f0837952e43f775c
piotrpdev <piotrpdev@gmail.com>
2024-03-16 22:36:00 +0000

    feat: add back theme switch in settings

--- app/src/main/java/dev/piotrp/melodyshare/fragments/SettingsFragment.kt
+++ app/src/main/java/dev/piotrp/melodyshare/fragments/SettingsFragment.kt
@@ -1,17 +1,22 @@
 package dev.piotrp.melodyshare.fragments
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
-import dev.piotrp.melodyshare.databinding.FragmentSecondBinding
+import com.github.ajalt.timberkt.i
+import dev.piotrp.melodyshare.MyApp
+import dev.piotrp.melodyshare.databinding.FragmentSettingsBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SettingsFragment : Fragment() {
-    private var _binding: FragmentSecondBinding? = null
+    private lateinit var app: MyApp
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,7 +27,7 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -31,10 +36,53 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        app = activity?.applicationContext as MyApp
+
+        binding.themeSwitch.setOnClickListener { onThemeSwitchToggle(it) }
+
+        updateSwitchBasedOnTheme()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateSwitchBasedOnTheme() {
+        val themeSwitch = binding.themeSwitch
+        themeSwitch.isSaveEnabled = false
+
+        when ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                i { "Dark mode detected, changing themeSwitch isChecked to true" }
+                themeSwitch.isChecked = true
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                i { "Light mode detected, changing themeSwitch isChecked to false" }
+                themeSwitch.isChecked = false
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
+        }
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun onThemeSwitchToggle(view: View) {
+        // TODO: Add persistence so once user sets a theme using
+        // the switch, it stays that way even upon restart.
+        var currentlyDark = false
+
+        when ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                currentlyDark = true
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                currentlyDark = false
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
+        }
+
+        i { "UI_MODE_NIGHT is currently set to $currentlyDark, attempting to invert..." }
+        AppCompatDelegate.setDefaultNightMode(if (currentlyDark) AppCompatDelegate.MODE_NIGHT_NO else AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
